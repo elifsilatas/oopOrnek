67b6f61bcf48c4134a6f67fded974f5301b508de
Elif Sıla Taş <122424595+elifsilatas@users.noreply.github.com>
2024-05-03 12:58:09 +0300

    oopOrnek

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Seven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            islem isl = new islem();
+            isl.topla(8, 2);
+            isl.fark(8, 2);
+            isl.carp(8, 2);
+            isl.bol(8, 2);
+            Console.Read();
+        }
+    }
+}
---
+++ islem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Seven
+{
+    internal class islem
+    {
+        public int topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            Console.Write("Toplam: " + toplam);
+            Console.WriteLine();
+            return toplam;
+        }
+
+        public int fark(int sayi1, int sayi2)
+        {
+            int cikar = sayi1 - sayi2;
+            Console.Write("Fark: " + cikar);
+            Console.WriteLine();
+            return cikar;
+        }
+
+        public int bol(int sayi1, int sayi2)
+        {
+            int bolme = sayi1 / sayi2;
+            Console.Write("Bölüm: " + bolme);
+            Console.WriteLine();
+            return bolme;
+        }
+
+        public int carp(int sayi1, int sayi2)
+        { 
+            int carpma = sayi1 * sayi2;
+            Console.Write("Çarpım: " + carpma);
+            Console.WriteLine();
+            return carpma;
+        }
+
+        
+    }
+}
